fcd4451a9a06fd6fd11a7d911407491920df2efa
marcusTL <mtlaursen1@gmail.com>
2020-09-21 14:04:41 +0200

    Last update for opg 3 - Marcus PS. dont fuck up

--- src/js/index.ts
+++ src/js/index.ts
@@ -5,7 +5,8 @@ new Vue({
     el: "#app",
     data: {
         name: "",
-        greeting: ""
+        greeting: "",
+        change: ""
     },
     methods: {
         sayHello() {
@@ -16,6 +17,42 @@ new Vue({
             else {
                 this.greeting = "Hello " + this.name
             }
+        },
+        determin(){
+            if(this.change=="SayHello"){
+                this.sayHello()
+            }
+            if(this.change=="Uppercase"){
+                this.uppercase()
+            }
+            if(this.change == "Lowercase"){
+                this.lowercase()
+            }
+            if(this.change == "Fuk"){
+                this.fukoff()
+            }
+            if(this.change == "FirstWord"){
+                this.first()
+            }
+            else{this.greeing = "invalid change"}
+
+        },
+        uppercase(){
+            let name = this.name.toUpperCase()
+            this.greeting = name
+        },
+        lowercase(){
+            let name = this.name.toLowerCase()
+            this.greeting = name
+        },
+        first(){
+             var str = this.name.split(" ")
+             this.greeting = str[0]
+             
+        },
+        fukoff(){
+            let name = this.name.replace(this.name,"f***")
+            this.greeting = name
         }
     }
 })
